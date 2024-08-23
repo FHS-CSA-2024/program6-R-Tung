98529ffe9166068db1c4e76ec2ff0aa119f9fc8b
R-Tung <107238461+R-Tung@users.noreply.github.com>
2024-08-22 22:08:57 -0700

    Update Program6.java
    finished program 6

--- Program6.java
+++ Program6.java
@@ -1,9 +1,36 @@
-//import stuff here!
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
-//Your code here
+public class Main {
+  public static void main(String[] args) {
 
-//Paste console output below:
+    final double pi = 3.14159;
+    
+    Scanner userRadius = new Scanner(System.in);
+    System.out.println("Enter radius: ");
+  
+    double radius = userRadius.nextDouble();
+    double diameter = radius * 2;
+    double area = pi * radius * radius;
+    double circumference = 2 * pi * radius;
+    
+    System.out.printf("Radius is: " + "%,.3f %n", + radius);
+    System.out.printf("Radius is: " + "%,.3f %n", + diameter);
+    System.out.printf("Radius is: " + "%,.3f %n", + area);
+    System.out.printf("Radius is: " + "%,.3f %n", + circumference);
+
+
+
+  }
+  
+}
+
 /*
-
+Enter radius: 
+3.712
+Radius is: 3.712 
+Radius is: 7.424 
+Radius is: 43.288 
+Radius is: 23.323 
 
 */
